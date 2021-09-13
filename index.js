7b81497df732ed4516fe3a55e38a6b989fb7ac4f
edisson <osorioedisson08@gmail.com>
2021-09-13 13:32:11 -0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello perra hp gonrrea');
+const generetePDF = async (name) => {
+    const { PDFDocument, rgb } = PDFLib;
+
+    const exBytes = await fetch("./bookii.pdf").then((res) => {
+        return res.arrayBuffer();
+    });
+    const pdfDoc = await PDFDocument.load(exBytes)
+     
+    const uri = await pdfDoc.saveAsBase64({dataUri: true})
+
+    document.querySelector("#mypdf").src = uri;
+     
+};
+
+generetePDF()
