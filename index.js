b2c8c6da9f6c27da99c249fc466c6d5de0684baa
edisson <osorioedisson08@gmail.com>
2021-09-13 15:07:49 -0500

    index.js

--- index.js
+++ index.js
@@ -5,29 +5,38 @@ const generetePDF = async (name) => {
         return res.arrayBuffer();
     });
 
-    const exFont = await fetch("./Sanchez-Regular.ttf").then(res =>{
+    const exFont = await fetch("./Sanchez-Regular.ttf").then(res => {
         return res.arrayBuffer()
     })
-    
+
     const pdfDoc = await PDFDocument.load(exBytes)
-    
+
     pdfDoc.registerFontkit(fontkit);
     const myFont = await pdfDoc.embedFont(exFont);
 
     const pages = pdfDoc.getPages();
     const firstPg = pages[0]
 
-    firstPg.drawText(name,{
+    firstPg.drawText(name, {
         x: 260,
         y: 350,
         size: 45,
         font: myFont,
     })
-  
-    const uri = await pdfDoc.saveAsBase64({dataUri: true})
-    
-    document.querySelector("#mypdf").src = uri;
-     
-};
 
-generetePDF("Edisson Osorio",)
+    const uri = await pdfDoc.saveAsBase64({ dataUri: true });
+    saveAs(uri, "Bookii Certificate.pdf", { autoBom: true });
+
+    // document.querySelector("#mypdf").src = uri;
+
+};
+const submitBtn = document.getElementById("submit")
+const inputVal = document.querySelector("#name")
+
+submitBtn.addEventListener("click", () => {
+    const val = inputVal.value;
+
+    generetePDF(val);
+})
+
+
